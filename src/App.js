b658e8159c33e1dcc5b2e89864a2d3093af456e6
jinglinliang <jliang54@wisc.edu>
2019-02-01 16:03:06 -0600

    Implemented a simple confirmation page and a status bar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import { DatePicker } from 'antd';
 import GoogleMapReact from 'google-map-react';
+
+import 'rc-steps/assets/index.css';
+import 'rc-steps/assets/iconfont.css';
+import Steps, { Step } from 'rc-steps';
+
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
 class App extends Component {
   static defaultProps = {
     center: {
@@ -28,4 +34,29 @@ class App extends Component {
   }
 }
 
-export default App;
+export class ConfirmationPage extends Component {
+
+    description = "这里可以添加描述";
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    Success! Your order have been placed
+                </h1>
+                <h2>
+                    Tracking number: 123456
+                </h2>
+                <Steps progressDot size="big" current={2}>
+                    <Step title="Order Received" description={this.description} />
+                    <Step title="Picked up" description={this.description} />
+                    <Step title="Delivering" description={this.description} />
+                    <Step title="Arrived" description={this.description} />
+                </Steps>
+            </div>
+        );
+    }
+}
+
+//export default App;
+export default ConfirmationPage;
