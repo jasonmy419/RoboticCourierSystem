c7871c402153dc1e6966d65f5bb6c98a01cb52ca
Yu Ma <jasonmy419@gmail.com>
2019-02-01 12:36:12 -0800

    set up react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { DatePicker } from 'antd';
+import GoogleMapReact from 'google-map-react';
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 class App extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+
+        <div style={{ height: '100vh', width: '100%' }}>
+          <DatePicker/>
+          <GoogleMapReact
+              bootstrapURLKeys={{ key: "AIzaSyD9rV0geR-5eRazBffY9G7fBJg5yC-G1qc" }}
+              defaultCenter={this.props.center}
+              defaultZoom={this.props.zoom}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
+            <AnyReactComponent
+                lat={59.955413}
+                lng={30.337844}
+                text={'Kreyser Avrora'}
+            />
+          </GoogleMapReact>
+        </div>
+
     );
   }
 }
