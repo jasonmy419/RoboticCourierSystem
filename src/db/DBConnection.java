3c3fa39d1d48da215c92de7b26f19724c22557f2
bliang3 <b7liang@eng.ucsd.edu>
2019-02-10 01:11:26 -0800

    DBConnection: feat: add getStatus func for track

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -2,30 +2,53 @@ package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DBConnection {
 
 	private Connection conn;	
-	
-    public DBConnection() {
-      	 try {
-      		 Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
-      		 conn = DriverManager.getConnection(DBUtility.URL);
-      		 
-      	 } catch (Exception e) {
-      		 e.printStackTrace();
-      	 }
-       }
-       
-       public void close() {
-      	 if (conn != null) {
-      		 try {
-      			 conn.close();
-      		 } catch (Exception e) {
-      			 e.printStackTrace();
-      		 }
-      	 }
-       }
 
-       // TODO
+	public DBConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
+			conn = DriverManager.getConnection(DBUtility.URL);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void close() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// TODO
+	public String getStatus(String orderID) {
+		if (conn == null) {
+			return null;
+		}
+		String status = "";
+		try {
+			String sql = "SELECT status FROM orders WHERE order_id = ?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, orderID);
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				status = rs.getString("status");
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return status;
+	}
+
 }
