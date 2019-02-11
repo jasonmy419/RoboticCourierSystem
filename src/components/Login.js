81139eb31fb766708e5482f9aada55186e49c450
liujz08 <jizhou.liu@aya.yale.edu>
2019-02-10 21:12:34 -0800

    add login component(data communication with backend to be completed)

---
+++ src/components/Login.js
@@ -0,0 +1,63 @@
+import React from 'react';
+//import {API_ROOT} from "./constants";
+import { Form, Icon, Input, Button, message,} from 'antd';
+
+class NormalLoginForm extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      // if (!err) {
+      //   console.log('Received values of form: ', values);
+      //   //send request
+      //   fetch(`${API_ROOT}/login`, {
+      //     method: 'POST',
+      //     body: JSON.stringify({
+      //       username: values.username,
+      //       password: values.password,
+      //     })
+      //   }).then((response) => {
+      //     if(response){
+      //       return response.text();
+      //     }
+      //     throw new Error(response.statusText);
+      //   }).then((data) => {
+      //     message.success('Registration Success');
+      //   }).catch((err) => {
+      //     console.log(err);
+      //     message.error('Registration Fail');
+      //   });
+      // }
+    });
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item className=".login-form">
+            {getFieldDecorator('username', {
+              rules: [{ required: true, message: 'Please input your username!' }],
+            })(
+                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+            )}
+          </Form.Item>
+          <Form.Item className=".login-form">
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: 'Please input your Password!' }],
+            })(
+                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+            )}
+          </Form.Item>
+          <Form.Item>
+            <Button type="default" htmlType="submit" className="login-form-button">
+              Log in
+            </Button>
+            Or <a href="">register now!</a>
+          </Form.Item>
+        </Form>
+    );
+  }
+}
+
+export const Login = Form.create({ name: 'normal_login' })(NormalLoginForm);
+
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { OrderInfo } from './OrderInfo';
 import { Map} from './Map';
-import { Register} from "./Register"
+import { Register } from "./Register"
+import { Login } from "./Login"
 
 export class Main extends React.Component {
     render() {
         return (
             <div className="main">
                 <Register/>
+                <Login/>
             </div>
         );
     }
