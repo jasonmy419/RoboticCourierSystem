e29f241c6a332341aed4e71a0c8012122839a62d
liujz08 <jizhou.liu@aya.yale.edu>
2019-02-10 21:06:10 -0800

    add register component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { OrderInfo } from './OrderInfo';
 import { Map} from './Map';
+import { Register} from "./Register"
+
 export class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                <OrderInfo/>
-                <Map/>
+                <Register/>
             </div>
         );
     }
