fbe4c8e0c29eb16e0c0e9d589bc8a081b7a34ce5
liujz08 <jizhou.liu@aya.yale.edu>
2019-02-10 22:42:58 -0800

    update package.json with react-router-dom installation; update Main.js with Router feature

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,12 +3,31 @@ import { OrderInfo } from './OrderInfo';
 import { Map} from './Map';
 import { Register } from "./Register"
 import { Login } from "./Login"
+import { Home } from './Home';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
 
 export class Main extends React.Component {
+    getLogin = () => {
+        return this.props.isLoggedIn ? <Redirect to="/home"/> : <Login handleSuccessfulLogin={this.props.handleSuccessfulLogin}/>;
+    }
+
+    getHome = () => {
+        return this.props.isLoggedIn ? <Home/> : <Redirect to="/login"/>;
+    }
+
     render() {
         return (
             <div className="main">
-                <Register/>
+              <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" render={this.getLogin}/>
+                    <Route path="/login" render={this.getLogin}/>
+                    <Route path="/register" component={Register}/>
+                    <Route path="/home" render={this.getHome}/>
+                    <Route render={this.getLogin}/>
+                </Switch>
+              </BrowserRouter>
             </div>
         );
     }
