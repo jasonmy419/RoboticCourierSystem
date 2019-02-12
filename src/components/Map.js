be59d8d63adf18173ddbe728d3aad53880dd4f16
Yu Ma <jasonmy419@gmail.com>
2019-02-11 16:20:35 -0800

    add three robots stations

--- src/components/Map.js
+++ src/components/Map.js
@@ -12,13 +12,35 @@ export class Map extends React.Component{
         lat: 37.773972,
         lng: -122.431297
       },
-      zoom: 13
+      zoom: 13,
+        firstStation:{
+          lat:37.793321,
+          lng:-122.422794
+        },
+        secondStation:{
+          lat:37.754538,
+            lng:-122.407157
+        },
+        thirdStation:{
+          lat: 37.757644,
+            lng:-122.436165
+        }
     };
     // _onClick = ({x, y, lat, lng, event, map, maps}) => {
     //     let marker = new maps.Marker({position: { lat: lat, lng:lng},map,title:'Hello World'})}
     renderMarkers(map, maps) {
-        let marker = new maps.Marker({
-            position: this.props.center,
+        let first = new maps.Marker({
+            position: this.props.firstStation,
+            map,
+            title: 'Hello World!'
+        });
+        let second = new maps.Marker({
+            position: this.props.secondStation,
+            map,
+            title: 'Hello World!'
+        });
+        let third = new maps.Marker({
+            position: this.props.thirdStation,
             map,
             title: 'Hello World!'
         });
