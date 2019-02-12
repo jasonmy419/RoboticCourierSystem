68d1590ad155bdeb33419177db3380297962082e
yaran zhang <yaranzhang@yarans-MacBook-Pro.local>
2019-02-11 19:41:22 -0500

    payment

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Switch, Route, Redirect } from "react-router-dom";
 import {Home} from "./Home";
 import { ConfirmationPage} from "./ConfirmationPage";
+import { Payment } from "./Payment";
 
 export class Main extends React.Component {
     render() {
         return (
             <div className="main">
                 <Switch>
-                    <Route exact path="/" render={this.getLogin}/>
+                    <Route exact path="/" component={Home}/>
                     {/*<Route path="/login" render={this.getLogin}/>*/}
                     {/*<Route path="/register" component={Register}/>*/}
                     {/*<Route path="/" component={Home}/>*/}
                     <Route path="/home" component={Home}/>
+                    <Route path="/payment" component={Payment}/>
                     <Route path="/confirmation" component={ConfirmationPage}/>
                     <Route component={Home}/>
                 </Switch>
