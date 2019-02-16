bb54b75ccb8ce77c08d37f098b8a942d6f045cf5
Binyi Han <dantengknight@gmail.com>
2019-02-16 10:18:32 -0800

    Signup: feat: add signup endpoint;       Login: feat: add login endpoint;     Logout: feat: add logout endpoint

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DBConnection {
 
@@ -51,4 +53,74 @@ public class DBConnection {
 		return status;
 	}
 
+	public boolean signup(String userId, String password, String firstname, String lastname) {
+		if (conn == null) {
+			return false;
+		}
+		try {
+			String sql = "INSERT INTO users (user_id, password, first_name, last_name) VALUES (?,?,?,?)";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, userId);
+			statement.setString(2, password);
+			statement.setString(3, firstname);
+			statement.setString(4, lastname);
+			int rs = statement.executeUpdate();
+			if (rs == 1) {
+				return true;
+			}
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+
+		return false; // Dummy
+	}
+
+	public String getFullname(String userId) {
+		// TODO Auto-generated method stub
+		if (conn == null) {
+			return "";
+		}
+		String name = "";
+
+		Set<String> categories = new HashSet<>();
+		try {
+			String sql = "SELECT first_name, last_name FROM users WHERE user_id = ? ";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, userId);
+			ResultSet rs = statement.executeQuery();
+			while (rs.next()) {
+				name = rs.getString("first_name") + " " + rs.getString("last_name");
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+		return name;
+	}
+
+	public boolean verifyLogin(String userId, String password) {
+		if (conn == null) {
+			return false;
+		}
+		try {
+			String sql = "SELECT user_id FROM users WHERE user_id = ? AND password = ?";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, userId);
+			statement.setString(2, password);
+
+			ResultSet rs = statement.executeQuery();
+			if (rs.next()) {
+				return true;
+			}
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+		return false;
+
+	}
+	
 }
---
+++ src/rpc/Signup.java
@@ -0,0 +1,78 @@
+package rpc;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import db.DBConnection;
+
+/**
+ * Servlet implementation class Signup
+ */
+@WebServlet("/signup")
+public class Signup extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Signup() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		DBConnection connection = new DBConnection();
+		try {
+			JSONObject input = RpcHelper.readJSONObject(request);
+			String userId = input.getString("user_id");
+			String password = input.getString("password");
+			String firstname = input.getString("first_name");
+			String lastname = input.getString("last_name");
+
+			JSONObject obj = new JSONObject();
+			if (connection.signup(userId, password, firstname, lastname)) {
+				/*
+				HttpSession session = request.getSession(); // Create session
+				session.setAttribute("user_id", userId); // Binds userId obj to session
+				session.setMaxInactiveInterval(6000);
+				*/
+				obj.put("status", "OK").put("user_id", userId).put("name", connection.getFullname(userId));
+			} else {
+				response.setStatus(401);
+				obj.put("status", "Invalid Information/User_id already exist");
+			}
+
+			RpcHelper.writeJsonObject(response, obj);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+	}
+
+}
