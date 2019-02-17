d57ee7da810039a635d618683ca27fd738051e5c
wyr <ywang5@sdsu.edu>
2019-02-16 21:56:28 -0800

    order address to server

--- src/components/OrderInfo.js
+++ src/components/OrderInfo.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Form, Input, Button, Select, Card, message } from 'antd';
-import { API_ROOT } from "../constants";
+import { Form, Input, Button, Select, Card, message, Radio } from 'antd';
+import { API_ROOT, proxyurl, NUMBER, WORD } from "../constants";
 import { Link } from 'react-router-dom';
 
 class OrderInfoForm extends React.Component {
-
+    state = {
+        pickUpAddr: [],
+        deliveryAddr: [],
+        value: 1,
+    }
     // handleSubmit = (e) => {
     //     e.preventDefault();
     //     this.props.form.validateFieldsAndScroll((err, values) => {
@@ -14,18 +18,59 @@ class OrderInfoForm extends React.Component {
     //         }
     //     });
     // }
+    onChange = (e) => {
+        console.log('radio checked', e.target.value);
+        this.setState({
+            value: e.target.value,
+        });
+    }
+
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.form.validateFieldsAndScroll((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
+                // check is valid addresses
+                let tmpPickingUp = this.isValidAddress(values.pickingUpAddress);
+                let tmpDelivery = this.isValidAddress(values.deliveryAddress);
+                if (!tmpPickingUp) {
+                    message.error('Picking up address is invalid!');
+                    return;
+                } else if (!tmpDelivery) {
+                    message.error('Delivery address is invalid!');
+                    return;
+                } else {
+                    this.setState({
+                        pickUpAddr : tmpPickingUp,
+                        deliveryAddr : tmpDelivery
+                    })
+                }
                 // send request
-                fetch(`${API_ROOT}/signup`, {
+                fetch(`${API_ROOT}/route`, {
                     method: 'POST',
+                    // body: JSON.stringify({
+                    //     username: values.pickingUpAddress,
+                    //     password: values.deliveryAddress,
+                    // }),
                     body: JSON.stringify({
-                        username: values.pickingUpAddress,
-                        password: values.deliveryAddress,
-                    }),
+                            'origin': {
+                                "street_number": "68",
+                                "street_name": "Willow Road",
+                                "city": "Menlo Park",
+                            },
+                            'waypoint': {
+                                street_number: this.state.pickUpAddr[0],
+                                street_name: this.state.pickUpAddr[1],
+                                city: this.state.pickUpAddr[2],
+                            },
+                            'destination': {
+                                street_number: this.state.deliveryAddr[0],
+                                street_name: this.state.deliveryAddr[1],
+                                city: this.state.deliveryAddr[2],
+                            },
+                            'mode': this.state.value,
+                        }
+                    ),
                 }).then((response) => {
                     if (response.ok) {
                         return response.text();
@@ -43,6 +88,28 @@ class OrderInfoForm extends React.Component {
                     });
             }
         });
+    }
+
+    isValidAddress = (address) => {
+        let building = this.getBuildingNumber(address);
+        let streetCity = this.getStreetCity(address);
+        if (building && streetCity) {
+            return [building, ...streetCity];
+        } else {
+            return false;
+        }
+    }
+
+    getBuildingNumber = (address) => {
+        let building = address.match(NUMBER);
+        if (!building) { return false; }
+        else { return building[0]; }
+    }
+
+    getStreetCity = (address) => {
+        let street = address.match(WORD);
+        if (!street || !street[1]) { return false; }
+        else { return [street[0], street[1]]; }
     }
 
     render() {
@@ -71,54 +138,57 @@ class OrderInfoForm extends React.Component {
             },
         };
 
-        const Option = Select.Option;
-
-        function handleChange(value) {
-            console.log(`selected ${value}`);
-        }
+        const RadioGroup = Radio.Group;
 
         return (
             <Card className="order">
-            <Form onSubmit={this.handleSubmit}>
-                <Form.Item
-                    {...formItemLayout}
-                    label="Picking Up Address"
-                >
-                    {getFieldDecorator('pickingUpAddress', {
-                        rules: [{ required: true, message: 'Please input your picking up address!', whitespace: true }],
-                    })(
-                        <Input />
-                    )}
-                </Form.Item>
-                <Form.Item
-                    {...formItemLayout}
-                    label="Delivery Address"
-                >
-                    {getFieldDecorator('deliveryAddress', {
-                        rules: [{ required: true, message: 'Please input your delivery address!', whitespace: true }],
-                    })(
-                        <Input />
-                    )}
-                </Form.Item>
-                <Form.Item
-                    {...formItemLayout}
-                    label="Recommanded By"
-                >
-                    <Select defaultValue="price" style={{ width: 315 }} onChange={handleChange}>
-                        <Option value="price">the lowest price</Option>
-                        <Option value="time">the shortest time</Option>
-                    </Select>
-                </Form.Item>
-                <Form.Item
-                    {...formItemLayout}
-                    label="Price: "
-                >
-                    <p>$100.00</p>
-                </Form.Item>
-                <Form.Item {...tailFormItemLayout}>
-                    <Button type="primary" htmlType="submit"><Link to="/payment">Place Order</Link></Button>
-                </Form.Item>
-            </Form>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Item
+                        {...formItemLayout}
+                        label="Picking Up Address"
+                    >
+                        {getFieldDecorator('pickingUpAddress', {
+                            rules: [{ required: true, message: 'Please input your picking up address!', whitespace: true }],
+                        })(
+                            <Input placeholder="#Building Street, City"/>
+                        )}
+                    </Form.Item>
+                    <Form.Item
+                        {...formItemLayout}
+                        label="Delivery Address"
+                    >
+                        {getFieldDecorator('deliveryAddress', {
+                            rules: [{ required: true, message: 'Please input your delivery address!', whitespace: true }],
+                        })(
+                            <Input placeholder="#Building Street, City"/>
+                        )}
+                    </Form.Item>
+                    <Form.Item
+                        {...formItemLayout}
+                        label="Recommanded By"
+                    >
+                        <RadioGroup onChange={this.onChange} value={this.state.value}>
+                            <Radio className="radio" value={1}>Shortest Time</Radio>
+                            <Radio className="radio" value={2}>Lowest Price</Radio>
+                            <Radio className="radio" value={3}>
+                                More...
+                                {this.state.value === 4 ? <Input style={{ width: 100, marginLeft: 10 }} /> : null}
+                            </Radio>
+                        </RadioGroup>
+                    </Form.Item>
+                    <Form.Item
+                        {...formItemLayout}
+                        label="Price: "
+                    >
+                        <p>$100.00</p>
+                    </Form.Item>
+                    <Form.Item {...tailFormItemLayout}>
+                        <Button type="primary" htmlType="submit">
+                            {/*<Link to="/payment">Place Order</Link>*/}
+                            Place Order
+                        </Button>
+                    </Form.Item>
+                </Form>
             </Card>
         );
     }
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,8 @@
-export const API_ROOT = 'https://around-75015.appspot.com/api/v1';
+// export const API_ROOT = 'https://around-75015.appspot.com/api/v1';
 export const ORDER_NUM = 'ORDER_NUM';
+
+export const API_ROOT = 'http://localhost:8080/RoboticCourierSystem';
+export const proxyurl = "https://cors-anywhere.herokuapp.com/";
+export const TOKEN_KEY = 'TOKEN_KEY';
+export const NUMBER = /[0-9]+/;
+export const WORD = /[a-zA-Z]+[a-zA-Z\s]*[a-zA-Z]+/gi;
