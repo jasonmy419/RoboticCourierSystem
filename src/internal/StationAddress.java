c01ed0f42aff2b97d57549e1a4a20b544c559000
Bing <brandonliang1994@gmail.com>
2019-02-18 19:05:55 -0800

    feature: fetch stations address from db

---
+++ src/internal/StationAddress.java
@@ -0,0 +1,35 @@
+package internal;
+
+import db.DBConnection;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import entity.Address;
+import external.CalculateFlightDistance;
+import external.DirectionsAPI;
+import rpc.RpcHelper;
+import entity.Address.AddressBuilder;
+import entity.Route;
+
+
+public class StationAddress {
+
+	public JSONArray getAddress() {
+		DBConnection conn = new DBConnection();
+		JSONArray address = null;
+		try {
+			address = conn.getStationAddress();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			conn.close();
+		}
+		
+		return address;
+	}
+
+}
