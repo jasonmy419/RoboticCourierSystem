152430cd1bc0829a637b20b5ade4ec3977200d1b
Bing <brandonliang1994@gmail.com>
2019-02-19 22:27:17 -0800

    feature: update the method to calculate price

--- src/internal/CalculatePrice.java
+++ src/internal/CalculatePrice.java
@@ -3,20 +3,23 @@ package internal;
 import org.json.JSONObject;
 
 import entity.TravelMode;
+import entity.ItemSize;
 import external.CalculateFlightDistance;
 import external.DirectionsAPI;
 import rpc.RpcHelper;
 
 public class CalculatePrice {
 
-	public static double getPrice(int time, int distance, TravelMode mode) {
+	public static double getPrice(int time, int distance, TravelMode mode, ItemSize size) {
 		double price = 0;
+		int priceOfSize = (size == ItemSize.SMALL) ? 2 : ((size == ItemSize.MEDIUM) ? 5 : 8);
+		
 		switch(mode) {
 		case FLYING: // this case is used on flying robots
-			price += time * 1.0 / 300 + 2 * distance * 1.0 / 1609.344;
+			price += time * 1.0 / 300 + 2 * distance * 1.0 / 1609.344 + priceOfSize;
 			break;
 		case WALKING: // this case is used for walking robots
-			price += time * 1.0 / 1200 + 5 * distance * 1.0 / 1609.344;
+			price += time * 1.0 / 1200 + 5 * distance * 1.0 / 1609.344 + priceOfSize;
 			break;
 		default:
 			System.out.println("Unable to calculate price!");
