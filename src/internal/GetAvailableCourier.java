ca491e4dbdc7804cc77b145c42f0a3ca82386517
Bing <brandonliang1994@gmail.com>
2019-02-23 01:02:15 -0800

    feature: add an inner method to get available couriers from each station

---
+++ src/internal/GetAvailableCourier.java
@@ -0,0 +1,31 @@
+package internal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import db.DBConnection;
+
+public class GetAvailableCourier {
+
+	public List<String> getAvailableCourier() {
+		DBConnection conn = new DBConnection();
+		List<String> list = new ArrayList<>();
+		List<String> stationList;
+		try {
+			stationList = conn.getStation();
+			for (String station : stationList) {
+				list.add(conn.getAvailableCourier(station));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			conn.close();
+		}
+//		for (String s : list) {
+//			System.out.println(s);
+//		}
+		return list;
+	}
+}
