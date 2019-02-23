416f9a0de5e0a989d63df966adda1349c24d0f57
Bing <brandonliang1994@gmail.com>
2019-02-22 22:49:35 -0800

    feature: add TIMESTAMP in courier table

--- src/db/DBTableCreation.java
+++ src/db/DBTableCreation.java
@@ -3,6 +3,9 @@ package db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Run this application to reset database schema
@@ -83,17 +86,21 @@ public class DBTableCreation {
 					+ ")";
 			statement.executeUpdate(sql);
 //
-//			sql = "CREATE TABLE couriers ("
-//					+ "courier_id VARCHAR(255) NOT NULL,"
+			sql = "CREATE TABLE couriers ("
+					+ "courier_id VARCHAR(255) NOT NULL,"
 //					+ "type VARCHAR(255) NOT NULL,"
 //					+ "price FLOAT,"
 //					+ "station VARCHAR(255) NOT NULL,"
 //					+ "order VARCHAR(255) NOT NULL,"
-//					// FIXME
-////					+ "PRIMARY KEY (courier_id, station),"
-////					+ "FOREIGN KEY (station_id) REFERENCES items(station_id)"
-//					+ ")";
-//			statement.executeUpdate(sql);
+					+ "time TIMESTAMP,"
+					// FIXME
+					+ "PRIMARY KEY (courier_id)"
+//					+ "FOREIGN KEY (station_id) REFERENCES items(station_id)"
+					+ ")";
+			statement.executeUpdate(sql);
+			
+			sql = "INSERT INTO couriers VALUES('111', CURRENT_TIMESTAMP)";
+			statement.executeUpdate(sql);
 			
 //			sql = "CREATE TABLE orders ("
 //					+ "order_id VARCHAR(255) NOT NULL,"
@@ -129,56 +136,56 @@ public class DBTableCreation {
 			sql = "INSERT INTO stations VALUES('33', 4605,'Morena Blvd','San Diego')";
 			statement.executeUpdate(sql);
 			
-			sql = "CREATE TABLE	payment (" 
-					 +"user_id VARCHAR(255) NOT NULL,"
-					 +"last_name VARCHAR(255) NOT NULL,"
-					 +"first_name VARCHAR(255) NOT NULL,"
-					 +"card_number VARCHAR(255) NOT NULL,"
-					 +"address_line1 VARCHAR(255) NOT NULL,"
-					 +"address_line2 VARCHAR(255),"
-					 +"city VARCHAR(255) NOT NULL,"
-					 +"zipcode INT NOT NULL,"
-					 +"state VARCHAR(255) NOT NULL,"
-					 +"month INT NOT NULL,"
-					 +"year INT NOT NULL,"
-					 +"cvv INT NOT NULL"
-					 +")";
-			
-			
-			statement.executeUpdate(sql);
-			
-			sql = "INSERT INTO payment VALUES('2233', 'wu', 'sicheng','xxxx-xxxx-xxxx-xxxx',"
-					+ " '3001 S. Michigan Ave', '', 'Chicago', 60616, 'IL', 04, 2018, 907)";
-			statement.executeUpdate(sql);
-			
-			
-			sql = "CREATE TABLE	orders (" 
-					 +"order_id VARCHAR(255) NOT NULL,"
-					 +"user_id VARCHAR(255) NOT NULL,"
-					 +"courier_id VARCHAR(255) NOT NULL,"
-					 +"item_id VARCHAR(255) NOT NULL,"
-					 +"type VARCHAR(255) NOT NULL,"
-					 +"start_street_number VARCHAR(255) NOT NULL,"
-					 +"start_street_name VARCHAR(255) NOT NULL,"
-					 +"start_city VARCHAR(255) NOT NULL,"
-					 +"end_street_number VARCHAR(255) NOT NULL,"
-					 +"end_street_name VARCHAR(255) NOT NULL,"
-					 +"end_city VARCHAR(255) NOT NULL,"
-					 +"status VARCHAR(255) NOT NULL,"
-					 +"route_duration DOUBLE NOT NULL,"
-					 +"route_distance DOUBLE NOT NULL,"
-					 +"route_price DOUBLE NOT NULL,"
-					 +"route_path VARCHAR(255) NOT NULL,"
-					 +"complete BOOLEAN"
-					 +")";
-			statement.executeUpdate(sql);
-			
-			sql = "INSERT INTO orders VALUES('sfogbwklskansbbvncs012e','123','xyz','111','D', '3869', 'Miramar St',"
-					+ " 'La Jolla', '4609', 'Convoy St', 'San Diego',"
-					+ "'TRANSIT',"
-				+ "996.0, 11.25, 29.83, 'k}qcFjushVf@QFABABAF?D?D@RBB@D?J?N?B?n@JLJJ@LDTDNDNDFBFBDBDBFBFBHB',"
-				+ "TRUE)";
-			statement.executeUpdate(sql);
+//			sql = "CREATE TABLE	payment (" 
+//					 +"user_id VARCHAR(255) NOT NULL,"
+//					 +"last_name VARCHAR(255) NOT NULL,"
+//					 +"first_name VARCHAR(255) NOT NULL,"
+//					 +"card_number VARCHAR(255) NOT NULL,"
+//					 +"address_line1 VARCHAR(255) NOT NULL,"
+//					 +"address_line2 VARCHAR(255),"
+//					 +"city VARCHAR(255) NOT NULL,"
+//					 +"zipcode INT NOT NULL,"
+//					 +"state VARCHAR(255) NOT NULL,"
+//					 +"month INT NOT NULL,"
+//					 +"year INT NOT NULL,"
+//					 +"cvv INT NOT NULL"
+//					 +")";
+//			
+//			
+//			statement.executeUpdate(sql);
+//			
+//			sql = "INSERT INTO payment VALUES('2233', 'wu', 'sicheng','xxxx-xxxx-xxxx-xxxx',"
+//					+ " '3001 S. Michigan Ave', '', 'Chicago', 60616, 'IL', 04, 2018, 907)";
+//			statement.executeUpdate(sql);
+//			
+//			
+//			sql = "CREATE TABLE	orders (" 
+//					 +"order_id VARCHAR(255) NOT NULL,"
+//					 +"user_id VARCHAR(255) NOT NULL,"
+//					 +"courier_id VARCHAR(255) NOT NULL,"
+//					 +"item_id VARCHAR(255) NOT NULL,"
+//					 +"type VARCHAR(255) NOT NULL,"
+//					 +"start_street_number VARCHAR(255) NOT NULL,"
+//					 +"start_street_name VARCHAR(255) NOT NULL,"
+//					 +"start_city VARCHAR(255) NOT NULL,"
+//					 +"end_street_number VARCHAR(255) NOT NULL,"
+//					 +"end_street_name VARCHAR(255) NOT NULL,"
+//					 +"end_city VARCHAR(255) NOT NULL,"
+//					 +"status VARCHAR(255) NOT NULL,"
+//					 +"route_duration DOUBLE NOT NULL,"
+//					 +"route_distance DOUBLE NOT NULL,"
+//					 +"route_price DOUBLE NOT NULL,"
+//					 +"route_path VARCHAR(255) NOT NULL,"
+//					 +"complete BOOLEAN"
+//					 +")";
+//			statement.executeUpdate(sql);
+//			
+//			sql = "INSERT INTO orders VALUES('sfogbwklskansbbvncs012e','123','xyz','111','D', '3869', 'Miramar St',"
+//					+ " 'La Jolla', '4609', 'Convoy St', 'San Diego',"
+//					+ "'TRANSIT',"
+//				+ "996.0, 11.25, 29.83, 'k}qcFjushVf@QFABABAF?D?D@RBB@D?J?N?B?n@JLJJ@LDTDNDNDFBFBDBDBFBFBHB',"
+//				+ "TRUE)";
+//			statement.executeUpdate(sql);
 
 	
 			conn.close();
