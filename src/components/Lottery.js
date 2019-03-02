d4758e7259c93b1d2b5403448e49faa8b087f38a
jinglinliang <jinglin@cs.wisc.edu>
2019-03-01 22:50:53 -0600

    lottery fetch v1

--- src/components/Lottery.js
+++ src/components/Lottery.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {API_ROOT} from "../constants";
+import {API_ROOT, USER_ID} from "../constants";
 import {Button, message} from 'antd';
 
 export class Lottery extends Component {
@@ -12,9 +12,35 @@ export class Lottery extends Component {
     draw_lottery = () => {
 
         let random =Math.floor(Math.random() * (50 - 5)) + 5;
-        this.setState({discount: random});
-        message.success(`${random}% of discount will add to your next order!`);
-        this.props.handleCouponDraw();
+
+        fetch(`${API_ROOT}/coupon`, {
+            method: 'POST',
+            body: JSON.stringify({
+                user_id: localStorage.getItem(USER_ID),
+                coupon: random
+            })
+        }).then((response) => {
+            if(response){
+                return response.text();
+            }
+            throw new Error(response.statusText);
+        }).then((data) => {
+            console.log(data);
+            return JSON.parse(data);
+        }).then((json) => {
+            if(json.status === "OK"){
+                console.log(json);
+                message.success(`${random}% of discount will add to your next order!`);
+                this.props.handleCouponDraw();
+            } else {
+                message.error('Error occurred');
+            }
+        }).catch((err) => {
+            console.log(err);
+            message.error('Failed to access to the website');
+        });
+        //this.setState({discount: random});
+
     }
 
 
