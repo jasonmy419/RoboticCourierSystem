68fe3d32c6a68f0a4afd860810e8f0ce63154d48
jinglinliang <jinglin@cs.wisc.edu>
2019-03-02 22:55:25 -0600

    binary lottery done

--- src/components/Lottery.js
+++ src/components/Lottery.js
@@ -6,38 +6,45 @@ import md5 from "md5";
 export class Lottery extends Component {
 
     state={
-        discount: 0,
+        discount: 5,
 
     }
 
-    draw_lottery = () => {
+    handleClick = () => {
 
-        let random = Math.floor(Math.random() * (50 - 5)) + 5;
-        this.setState({discount: random});
-        message.success(`${random}% of discount will add to your next order!`);
-        this.props.handleCouponDraw();
-        console.log("coupon", JSON.stringify({
-            user_id: localStorage.getItem(USER_ID),
-            coupon: random,
-        }))
-        fetch(`${API_ROOT}/coupon`, {
-            method: 'POST',
-            body: JSON.stringify({
-                user_id: localStorage.getItem(USER_ID),
-                coupon: random
-            })
-        }).then((response) => {
-            if(response){
-                return response.text();
-            }
-            throw new Error(response.statusText);
-        }).then((data) => {
-            console.log(data);
-            return JSON.parse(data);
-        }).then((json) => {
-            if(json.status === "OK") {
-                console.log(json);
-            }})}
+        var random_boolean = Math.random() >= 0.5;
+
+        if (random_boolean) {
+            this.setState(prev => ({discount: prev.discount + 5}));
+        } else {
+            fetch(`${API_ROOT}/coupon`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    user_id: localStorage.getItem(USER_ID),
+                    coupon: this.state.discount
+                })
+            }).then((response) => {
+                if(response){
+                    return response.text();
+                }
+                throw new Error(response.statusText);
+            }).then((data) => {
+                console.log(data);
+                return JSON.parse(data);
+            }).then((json) => {
+                if(json["lottery:"] === "success") {
+                    console.log(json);
+                    message.success(`${this.state.discount}% of discount will add to your next order!`);
+                    this.props.handleCouponDraw();
+                }})
+
+        }
+        // console.log("coupon", JSON.stringify({
+        //     user_id: localStorage.getItem(USER_ID),
+        //     coupon: random,
+        // }))
+
+    }
 
 
     render() {
@@ -47,9 +54,11 @@ export class Lottery extends Component {
                 <h1> Congratulation! You win a chance to draw a coupon for your next order! </h1>
                 <div className="lottery-button">
                     <div className="lottery-instruction">
-                        <h2> Click the button to draw: </h2>
+                        <h2> Keep clicking to draw: {this.state.discount}%</h2>
                     </div>
-                    <Button type="primary" onClick={this.draw_lottery}>Click me!</Button>
+
+                    <Button type="primary" onClick={this.handleClick}>Click me!</Button>
+
                 </div>
             </div>
         );
